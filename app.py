7696cc42e6db4c34cac7bb3d3b23232893479fca
asadsoli <lionzamlka@gmail.com>
2026-05-05 18:09:53 +0300

    Set up Flask app with bot integration

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+from flask import Flask
+import threading
+from bot import start_bot
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "ULTRA V10 BOT IS RUNNING ✔"
+
+def run_bot():
+    start_bot()
+
+if __name__ == "__main__":
+    port = 10000
+    threading.Thread(target=run_bot).start()
+    app.run(host="0.0.0.0", port=port)
