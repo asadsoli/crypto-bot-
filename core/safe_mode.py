066b3a4adb06472868b3b39d82e5a177c9fc7820
asadsoli <lionzamlka@gmail.com>
2026-05-20 03:56:52 +0300

    Delete core/safe_mode.py

--- core/safe_mode.py
+++
@@ -1,9 +0,0 @@
-class SafeUpgradeSystem:
-    def __init__(self):
-        self.lock = True
-
-    def enable_safe_mode(self):
-        print("🛡 SAFE MODE ENABLED")
-
-    def disable_safe_mode(self):
-        print("⚠ SAFE MODE DISABLED")
